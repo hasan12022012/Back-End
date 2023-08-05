51f27d73039a06624322836b0cdc97e617351048
Hasan Huseynli <hasansh@code.edu.az>
2023-08-05 19:44:59 +0400

    SomeUpdates

--- Backend/Backend/Controllers/BlogController.cs
+++ Backend/Backend/Controllers/BlogController.cs
@@ -19,15 +19,103 @@ namespace Backend.Controllers
         {
             IEnumerable<Blog> blogs = await _appDbContext.Blogs
                 .Where(m => !m.IsDeleted)
+                .Include(m => m.BlogTags)
                 .Include(m => m.BlogCategory)
+                .OrderByDescending(m => m.Id)
+                .ToListAsync();
+
+            IEnumerable<BlogCategory> blogCategories = await _appDbContext.BlogCategories
+                .Where(m => !m.IsDeleted)
+                .Include(m => m.Blogs)
+                .ToListAsync();
+
+            IEnumerable<Tag> tags = await _appDbContext.Tags
+                .Where(m => !m.IsDeleted)
                 .ToListAsync();
 
             BlogVM model = new()
             {
-                Blogs = blogs
+                Blogs = blogs,
+                BlogCategories = blogCategories,
+                Tags = tags
             };
 
             return View(model);
+        }
+
+        public async Task<IActionResult> Search(string search)
+        {
+            BlogVM searchBlog = new();
+
+            if (search != null)
+            {
+                searchBlog.Blogs = await _appDbContext.Blogs
+                    .Where(m => !m.IsDeleted && m.Name.ToLower()
+                    .Contains(search.ToLower()) && !m.IsDeleted)
+                    .Include(m => m.BlogTags)
+                    .Include(m => m.BlogCategory)
+                    .ToListAsync();
+            }
+            else
+            {
+                searchBlog.Blogs = await _appDbContext.Blogs
+                    .Where(m => !m.IsDeleted)
+                    .Include(m => m.BlogTags)
+                    .Include(m => m.BlogCategory)
+                    .ToListAsync();
+            }
+
+            return PartialView("_BlogsPartial", searchBlog);
+        }
+
+        public async Task<IActionResult> FilterTag(int id)
+        {
+            BlogVM model = new();
+
+            if (id != null)
+            {
+                model.Blogs = await _appDbContext.Blogs
+                    .Where(m => !m.IsDeleted && m.BlogTags
+                    .Where(m => m.TagId == id)
+                    .FirstOrDefault().TagId == id)
+                    .Include(m => m.BlogTags)
+                    .Include(m => m.BlogCategory)
+                    .ToListAsync();
+            }
+            else
+            {
+                model.Blogs = await _appDbContext.Blogs
+                    .Where(m => !m.IsDeleted)
+                    .Include(m => m.BlogTags)
+                    .Include(m => m.BlogCategory)
+                    .ToListAsync();
+            }
+
+            return PartialView("_BlogsPartial", model);
+        }
+
+        public async Task<IActionResult> FilterCategory(int id)
+        {
+            BlogVM model = new();
+
+            if (id != null)
+            {
+                model.Blogs = await _appDbContext.Blogs
+                    .Where(m => !m.IsDeleted && m.BlogCategoryId == id)
+                    .Include(m => m.BlogTags)
+                    .Include(m => m.BlogCategory)
+                    .ToListAsync();
+            }
+            else
+            {
+                model.Blogs = await _appDbContext.Blogs
+                    .Where(m => !m.IsDeleted)
+                    .Include(m => m.BlogTags)
+                    .Include(m => m.BlogCategory)
+                    .ToListAsync();
+            }
+
+            return PartialView("_BlogsPartial", model);
         }
 
         public IActionResult Detail()
--- Backend/Backend/ViewModels/BlogVM.cs
+++ Backend/Backend/ViewModels/BlogVM.cs
@@ -5,5 +5,8 @@ namespace Backend.ViewModels
     public class BlogVM
     {
         public IEnumerable<Blog>? Blogs { get; set; }
+        public IEnumerable<BlogCategory>? BlogCategories { get; set; }
+        public IEnumerable<Tag>? Tags { get; set; }
+        public Blog? Blog { get; set; }
     }
 }
